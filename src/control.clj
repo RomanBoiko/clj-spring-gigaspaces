c4190021e72773973ec5b1d5ec7335ab4e5742b5
boikoro <boiko.roman@gmail.com>
2014-02-04 23:30:48 +0000

    inventory message parsing examples added

--- src/control.clj
+++ src/control.clj
@@ -1,4 +1,7 @@
 (ns control
+	(:require
+		[clojure.test :refer :all]
+		[clojure.xml :refer [parse]])
 	(:use clojure.test)
 	(:gen-class
 		:name ControlListener
@@ -9,12 +12,34 @@
 (defn class-listener-onEvents [event]
 	(println (str "=============>" event)))
 
+(defn xmlid-to-id [xmlid]
+	(let [attrs (:attrs xmlid)]
+		[(:domain attrs) (first (:content xmlid)) (:version attrs)]))
+
+(defn extract-inventory-ids [inventory-xml-source]
+	(->>
+		(parse inventory-xml-source)
+		(xml-seq)
+		(first)
+		(:content)
+		(map xmlid-to-id)))
+
+(defn extract-inventory-ids-lisp-style [inventory-xml-source]
+	(map xmlid-to-id
+		(:content
+				(first (xml-seq (parse inventory-xml-source))))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;TESTS;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(deftest test-eq
-	(is (= 2 2)))
+(def test-inventory-ids [["FO" "111" "1"] ["FO" "222" "2"]])
+
+(deftest should-extract-ids-from-inventory-message
+	(is
+		(= ["FO" "111" "1"] (xmlid-to-id {:tag :id, :attrs {:domain "FO", :version "1"}, :content ["111"]})))
+	(is
+		(= test-inventory-ids (extract-inventory-ids (clojure.java.io/file "resources/inventory-message-example.xml")))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;AUXILIARY;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
